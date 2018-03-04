447783567eb2d4ddafbd4829a09735c4dab40b80
TineZata <tinezata@googlemail.com>
2018-03-04 20:23:21 +0000

    slimmed and working

--- vue-signalR-epicsSharp/ClientApp/components/app-root.vue
+++ vue-signalR-epicsSharp/ClientApp/components/app-root.vue
@@ -1,9 +1,6 @@
 <template>
     <div id="app" class="container-fluid">
         <div class="row">
-            <div class="col-sm-3">
-                <nav-menu params="route: route"></nav-menu>
-            </div>
             <div class="col-sm-9">
                 <router-view></router-view>
             </div>
@@ -16,10 +13,8 @@
 <script>
 import Vue from 'vue'
 import HomePage from './home-page'
-import NavMenu from './nav-menu'
 
 Vue.component('home-page', HomePage);
-Vue.component('nav-menu', NavMenu);
 
 export default {
     data() {
